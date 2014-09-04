8b639ca238d9199e61b70d98af6a908cb31642ca
Hanh Kieu <hckieu@ucdavis.edu>
2014-09-04 03:31:27 -0700

    added admin pages to question and choice

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from polls.models import Question, Choice
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
 
+
+admin.site.register(Question, QuestionAdmin)
 # Register your models here.
+admin.site.register(Choice)
